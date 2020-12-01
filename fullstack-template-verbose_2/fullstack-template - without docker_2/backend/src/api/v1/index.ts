efe4b87eda0559c386dca16c9a556eaa68503cdc
Vasilis Tampakopoulos <csd3853@csd.uoc.gr>
2020-12-01 18:23:24 +0200

    notinV2

--- fullstack-template-verbose_2/fullstack-template - without docker_2/backend/src/api/v1/index.ts
+++ fullstack-template-verbose_2/fullstack-template - without docker_2/backend/src/api/v1/index.ts
@@ -19,7 +19,7 @@ import { SmartphonePlayersController } from './smartphonePlayers/smartphonePlaye
 import { SmartphoneProfileController } from './smartphoneProfile/smartphoneProfile.controller';
 import { TwitterSmartphoneController } from './twitterSmartphone/twitterSmartphone.controller';
 import { VoteSmartphoneController } from './voteSmartphone/voteSmartphone.controller';
-// import { VoteDoneSmartphoneController } from './voteDoneSmartphone/voteDoneSmartphone.controller';
+import { VoteDoneSmartphoneController } from './voteDoneSmartphone/voteDoneSmartphone.controller';
 import { SmartableHomeController } from './smartableHome/smartableHome.controller';
 import { SmartableVideosController } from './smartableVideos/smartableVideos.controller';
 import { WallHomeController } from './wallHome/wallHome.controller';
@@ -134,17 +134,17 @@ apiV1Router
   )
 
   // smartphone players routes
-  /*.use(
+  .use(
     '/voteDoneSmartphone',
-     new VoteDoneSmartphoneController().applyRoutes()
-  )*/
-
+    new VoteDoneSmartphoneController().applyRoutes()
+  )
+  
   // smartable home
   .use(
     '/smartableHome',
     new SmartableHomeController().applyRoutes()
   )
-
+  
   // smartable videos
   .use(
     '/smartableVideos',
