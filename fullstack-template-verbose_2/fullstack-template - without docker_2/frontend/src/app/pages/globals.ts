5b8306d610b3e32e82d26e34c3d53cabe942fe90
Ioannis Kyrosis <csd3611@csd.uoc.gr>
2020-12-14 17:39:26 +0200

    global variables for videos progress

--- fullstack-template-verbose_2/fullstack-template - without docker_2/frontend/src/app/pages/globals.ts
+++ fullstack-template-verbose_2/fullstack-template - without docker_2/frontend/src/app/pages/globals.ts
@@ -3,4 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class Globals {
     tweets: any[] = [];
+    currentVideo = 0;
+    currentVideoPlayer = 0;
+    paused = 0;
 }
--- fullstack-template-verbose_2/fullstack-template - without docker_2/frontend/src/app/pages/tvhighlights/tvhighlights.component.ts
+++ fullstack-template-verbose_2/fullstack-template - without docker_2/frontend/src/app/pages/tvhighlights/tvhighlights.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SmartableVideosService } from 'src/app/global/services/SmartableVideos/SmartableVideos.service';
 import { SocketsService } from 'src/app/global/services';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Globals } from '../globals';
 
 @Component({
   selector: 'ami-fullstack-tvhighlights',
@@ -14,14 +15,8 @@ export class TVHighlightsComponent implements OnInit {
   /**previous playing video number */
   public previous = 0;
 
-  /**currently playing video number */
-  public current = 0;
-
-  /**indicates if video is paused (1) or not (0) */
-  public paused;
-
   constructor(private route:ActivatedRoute, private smartableVideosService: SmartableVideosService,
-    private socketService: SocketsService) {
+    private socketService: SocketsService, private globals: Globals) {
       this.socketEvents = [];
   }
 
@@ -30,7 +25,7 @@ export class TVHighlightsComponent implements OnInit {
     this.socketService.syncMessages("play video").subscribe(msg => {
       
       /**update video style and hide buttons if smartable called playVideo() */
-      if(msg.message.device != 2 && this.current!=0) {
+      if(msg.message.device != 3) {
 
         console.log('nyees');
 
@@ -43,24 +38,35 @@ export class TVHighlightsComponent implements OnInit {
 
         /**reset previous */
         this.previous = 0;
-
-        /**reset current */
-        this.current = 0;
       }
     })
+
+    if(this.globals.currentVideoPlayer == 3){
+      /**show pause/play button*/
+      if(this.globals.paused == 0){
+        (<HTMLInputElement>document.getElementById("pause_"+this.globals.currentVideo)).style.visibility= 'visible';
+      } else {
+        (<HTMLInputElement>document.getElementById("play_"+this.globals.currentVideo)).style.visibility= 'visible';
+      }
+
+      /**change border color of thumbnail */
+      (<HTMLInputElement>document.getElementById("thumbnail_"+this.globals.currentVideo)).style.borderStyle= 'solid';
+      (<HTMLInputElement>document.getElementById("thumbnail_"+this.globals.currentVideo)).style.borderColor= '#DB00FF';
+    }
   }
 
   /*play video to wall function */
   public playVideo(url, number, device){
       
-    if(number != this.current) {
+    if((number != this.globals.currentVideo && device == this.globals.currentVideoPlayer) 
+        || (device != this.globals.currentVideoPlayer)) {
 
       console.log('play video please');
 
-      /**set current */
-      this.current=number;
-
-      this.paused = 0;
+      /**set global variables */
+      this.globals.currentVideo = number;
+      this.globals.currentVideoPlayer = device;
+      this.globals.paused = 0;
 
       /**show buttons (pause, screenshot, record) */
       (<HTMLInputElement>document.getElementById("pause_"+number)).style.visibility= 'visible';
@@ -88,18 +94,18 @@ export class TVHighlightsComponent implements OnInit {
   /**pause video */
   public pause_play(number){
     
-    if(this.current == number){
+    if(this.globals.currentVideo == number && this.globals.currentVideoPlayer == 3){
       /**visibilities */
-      if(this.paused == 0 ){
+      if(this.globals.paused == 0 ){
         console.log('pause please');
         (<HTMLInputElement>document.getElementById("pause_"+number)).style.visibility = "hidden" ;
         (<HTMLInputElement>document.getElementById("play_"+number)).style.visibility = "visible" ;
-        this.paused = 1 ;
+        this.globals.paused = 1 ;
       } else {
         console.log('play please');
         (<HTMLInputElement>document.getElementById("pause_"+number)).style.visibility = "visible" ;
         (<HTMLInputElement>document.getElementById("play_"+number)).style.visibility = "hidden" ;
-        this.paused = 0 ;
+        this.globals.paused = 0 ;
       }
       /**call pause_play() from service */
       this.smartableVideosService.pause_play(number).subscribe();
