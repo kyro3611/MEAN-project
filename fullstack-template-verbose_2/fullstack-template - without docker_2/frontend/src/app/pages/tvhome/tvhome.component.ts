c262449bc17acc60191767090180b6279aebfc28
Ioannis Kyrosis <csd3611@csd.uoc.gr>
2020-12-16 02:17:22 +0200

    watch live from tvhome

--- fullstack-template-verbose_2/fullstack-template - without docker_2/frontend/src/app/pages/tvhome/tvhome.component.ts
+++ fullstack-template-verbose_2/fullstack-template - without docker_2/frontend/src/app/pages/tvhome/tvhome.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SmartableVideosService } from 'src/app/global/services/SmartableVideos/SmartableVideos.service';
 import { SocketsService } from 'src/app/global/services';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Globals } from '../globals';
 
 @Component({
   selector: 'ami-fullstack-tvhome',
@@ -8,10 +10,20 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['../../../assets/cssTV/homestyle.css', '../../../assets/cssTV/headerstyle.css']
 })
 export class TVHomeComponent implements OnInit {
+  public socketEvents: { event: string, message: any }[];
 
-  constructor() { }
+  constructor(private route:ActivatedRoute, private smartableVideosService: SmartableVideosService,
+    private socketService: SocketsService, private globals: Globals) {
+      this.socketEvents = [];
+  }
 
   ngOnInit() {
   }
 
+  /*play video to wall function */
+  public playVideo(url, number, device){
+    console.log('play video please');
+    /**call playVideo() from service */
+    this.smartableVideosService.playVideo(url, number, device).subscribe();
+  }
 }
